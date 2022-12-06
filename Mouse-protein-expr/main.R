4fba337e1e8dfc49e019a52da05a42b2545d1a7c
Yogesh Maithania <maithania.yo@northeastern.edu>
2022-12-05 20:49:05 -0500

    rough script

--- Mouse-protein-expr/main.R
+++ Mouse-protein-expr/main.R
@@ -1,5 +1,33 @@
 ## main script 
 
+rm(list = ls())
+####### pacakges #######
+packages <- c("dplyr", "tidyr", "psych", "corrplot", "ROSE", "nnet", "neuralnet",
+              "e1071", "caret", "rpart","randomForest", "ipred")
+# dplyr: for data manipulation
+# tidyr: or data wrangling
+# psych: for pairs panel plot
+# corrplot: for correlation matrix
+# ROSE: for imputing imbalanced classes
+# nnet: for NN modeling
+# neuralnet: for NN modeling
+# e1071: for SVM implementation
+# caret: for data preparation and evaluation of model
+# rpart: for decision tree regularization
+# randomForest: for ensemble model-1 (random forest)
+# ipred: for ensemble model-2 (bagging)
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
+
+
+######### data review ########
 nc <- read.csv("Data_Cortex_Nuclear.csv", header = TRUE)
 
 summary(nc)
@@ -11,9 +39,10 @@ table(nc$Behavior)
 table(nc$class)
 # check categorical distribution
 
-library(rpart)
+########## pre-processing #########
 
-ncClass <- nc$class
+
+
 
 
 
